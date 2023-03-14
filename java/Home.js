87e2f402669c1a38b65ceef19d3b1a97bb7202d9
spawnew <96084099+spawnew@users.noreply.github.com>
2023-03-14 18:23:05 -0300

    Update Home.js

--- java/Home.js
+++ java/Home.js
@@ -24,26 +24,27 @@ function crearlista (arr){ // crear la lista y la mete en las cartas
     lista.innerHTML=""
     arr.events.forEach ( car => {
     let div=document.createElement("div")
-    
-        
-        
-           
+      
             let titulo = document.createElement('h5')
             let foto=document.createElement('img')
             let precio=document.createElement('h7')
-            let newLink = document.createElement("a")
+            
             let fecha=document.createElement('h7')
             let categoria=document.createElement('h7') 
+            let description = document.createElement('p')
             fecha.textContent=car.date
-            newLink.href = "./Detail.html"
-newLink.innerText = "Ver mas.."      
+            let link = document.createElement('a')
+        link.textContent = 'ver mas'
+        link.href = `./detail.html?id=${car._id}`
+           
       div.className="card"
           categoria.textContent=car.category
-            foto.src=car.image
+            foto.src=` ${car.image}`
             foto.className = "card-img-top"
             titulo.className="card.title"
           precio.className="card-body"
-         
+           description.textContent=car.description
+           description.className="descripcion"
           precio.innerText="price"+" "+"$"+car.price
         titulo.textContent=car.name
        
@@ -76,13 +77,33 @@ const formu=document.getElementById('formulario')
          
            
         
-        
-        
-        
-    }
-        
-    lista.appendChild(div)
-   )
+   
+    console.log(encontre)
+    crearlista(encontre);
+     })
+
+
+     let box = document.querySelectorAll("input[type='checkbox']")
+
+console.log(box)
+box.forEach(boton =>boton.addEventListener('change',filtrar )) //a cada check le agrega el addevent y escucha cada vez q cambia el valor y ejecute la funcion
 
 
 
+
+function filtrar (){
+    let filtrado = Array.from(box).filter(checkbox => checkbox.checked)//ACA ME DA LOS checkbox q estan chequeados
+    console.log(filtrado[0].value)
+
+let encontre1= filtro(data.events,filtrado[0].value)
+  crearlista(encontre1)  
+}
+
+function filtro(arr,value){
+
+    let filtrex=arr.filter(arr=>(arr.category.includes(value)))
+
+
+
+return filtrex;
+}
